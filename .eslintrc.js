c0f39b5584d1bf541d8b1928223a4293b23b1640
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-09-07 17:41:03 -0300

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	files: ["src/**/*.ts"],
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: "eslint:recommended",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+};
