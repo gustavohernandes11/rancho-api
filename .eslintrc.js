22b264b5f9d1efc6b97cc6187969c38d9e7a9a12
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-09-07 18:02:42 -0300

    chore: add husky command to lint staged files and test before commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,19 @@
 module.exports = {
-	files: ["src/**/*.ts"],
 	env: {
 		browser: true,
 		es2021: true,
 	},
 	extends: "eslint:recommended",
+
+	overrides: [
+		{
+			files: ["src/*.js", "src/*.ts"],
+			rules: {
+				quotes: ["error", "single"],
+			},
+		},
+	],
+	ignorePatterns: ".eslintrc.js",
 	parserOptions: {
 		ecmaVersion: "latest",
 		sourceType: "module",
