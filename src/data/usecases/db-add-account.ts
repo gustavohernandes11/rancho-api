a342999e81d37b7fbda04fc9116bc5983333e793
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-09-08 20:15:05 -0300

    feat: ensure DbAddAccount uses the correct properties

--- src/data/usecases/db-add-account.ts
+++ src/data/usecases/db-add-account.ts
@@ -5,7 +5,9 @@ export class DbAddAccount implements IAddAccount {
 	constructor(private readonly addAccountRepository: IAddAccountRepository) {}
 
 	async add(account: IAccount): Promise<boolean> {
-		const success = await this.addAccountRepository.add(account);
+		const success = await this.addAccountRepository.add(
+			Object.assign(account, { password: "hashed_password" })
+		);
 		return new Promise((resolve) => resolve(true));
 	}
 }
