dcaa85ef65580a4cf09e3eab5c1404b129cccfee
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-09-12 15:24:08 -0300

    refact: group up sut instantiation into a single factory method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -4,11 +4,23 @@ import { IHttpRequest } from "../protocols/http";
 import { SigunUpController } from "./signup";
 
 describe("Signup Controller", () => {
-	it("should return 400 if name is not provided", async () => {
-		const validations = [new RequiredFieldValidator("name")];
-		const validationStub = new ValidationComposite(validations);
+	interface ISutTypes {
+		sut: SigunUpController;
+	}
+	const makeSut = (): ISutTypes => {
+		const validations = [
+			new RequiredFieldValidator("name"),
+			new RequiredFieldValidator("email"),
+			new RequiredFieldValidator("password"),
+			new RequiredFieldValidator("passwordConfirmation"),
+		];
+		const singUpValidationComposite = new ValidationComposite(validations);
 
-		const sut = new SigunUpController(validationStub);
+		const sut = new SigunUpController(singUpValidationComposite);
+		return { sut };
+	};
+	it("should return 400 if name is not provided", async () => {
+		const { sut } = makeSut();
 		const httpRequest: IHttpRequest = {
 			body: {
 				email: "valid_email@gmail.com",
@@ -20,10 +32,7 @@ describe("Signup Controller", () => {
 		expect(response.statusCode).toBe(400);
 	});
 	it("should return 400 if email is not provided", async () => {
-		const validations = [new RequiredFieldValidator("email")];
-		const validationStub = new ValidationComposite(validations);
-
-		const sut = new SigunUpController(validationStub);
+		const { sut } = makeSut();
 		const httpRequest: IHttpRequest = {
 			body: {
 				name: "valid_name@gmail.com",
@@ -35,10 +44,7 @@ describe("Signup Controller", () => {
 		expect(response.statusCode).toBe(400);
 	});
 	it("should return 400 if password is not provided", async () => {
-		const validations = [new RequiredFieldValidator("password")];
-		const validationStub = new ValidationComposite(validations);
-
-		const sut = new SigunUpController(validationStub);
+		const { sut } = makeSut();
 		const httpRequest: IHttpRequest = {
 			body: {
 				name: "valid_name@gmail.com",
@@ -50,12 +56,7 @@ describe("Signup Controller", () => {
 		expect(response.statusCode).toBe(400);
 	});
 	it("should return 400 if passwordConfirmation is not provided", async () => {
-		const validations = [
-			new RequiredFieldValidator("passwordConfirmation"),
-		];
-		const validationStub = new ValidationComposite(validations);
-
-		const sut = new SigunUpController(validationStub);
+		const { sut } = makeSut();
 		const httpRequest: IHttpRequest = {
 			body: {
 				name: "valid_name@gmail.com",
