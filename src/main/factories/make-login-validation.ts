3a340b40982a30ad59278f2075e9df0c40b53366
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-09-21 14:55:16 -0300

    chore: rename factory function

---
+++ src/main/factories/make-login-validation.ts
@@ -0,0 +1,16 @@
+import {
+	RequiredFieldValidation,
+	EmailValidation,
+	ValidationComposite,
+} from "../../validation/validators";
+import { EmailValidateAdapter } from "../adapters/email-validate-adapter";
+
+export const makeLoginValidations = () => {
+	const validations = [
+		new RequiredFieldValidation("email"),
+		new RequiredFieldValidation("password"),
+		new EmailValidation("email", new EmailValidateAdapter()),
+	];
+
+	return new ValidationComposite(validations);
+};
