19fff7bc1aa95dec328b5ae0f34c6e00648dc6a5
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-10-16 16:06:33 -0300

    test: ensure decrypt method behavior

--- src/infra/criptography/jwt-adapter.spec.ts
+++ src/infra/criptography/jwt-adapter.spec.ts
@@ -5,6 +5,9 @@ jest.mock("jsonwebtoken", () => ({
 	async sign(): Promise<string> {
 		return new Promise((resolve) => resolve("encrypted_text"));
 	},
+	async verify(): Promise<string> {
+		return new Promise((resolve) => resolve("decrypted_text"));
+	},
 }));
 
 describe("JWT Adapter", () => {
@@ -35,6 +38,35 @@ describe("JWT Adapter", () => {
 			});
 			const promise = sut.encrypt("any_text");
 
+			expect(promise).rejects.toThrow();
+		});
+	});
+	describe("decrypt", () => {
+		it("should call decrypt with correct values", async () => {
+			const secret = "any_secret";
+			const sut = new JwtAdapter(secret);
+			const jwtSpy = jest.spyOn(sut, "decrypt");
+			const text = "any_text";
+			await sut.decrypt(text);
+
+			expect(jwtSpy).toHaveBeenCalledWith("any_text");
+			expect(jwtSpy).toHaveBeenCalled();
+		});
+		it("should return an encrypted value on success", async () => {
+			const secret = "any_secret";
+			const sut = new JwtAdapter(secret);
+			const response = await sut.decrypt("any_text");
+
+			expect(response).toBe("decrypted_text");
+		});
+
+		it("should throw if decrypt throws", () => {
+			const secret = "any_secret";
+			const sut = new JwtAdapter(secret);
+			jest.spyOn(jwt, "verify").mockImplementationOnce(() => {
+				throw new Error();
+			});
+			const promise = sut.decrypt("any_text");
 			expect(promise).rejects.toThrow();
 		});
 	});
