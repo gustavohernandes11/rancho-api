bc2ef262ee07d303264b432563640c0b5392dbb3
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-10-16 13:54:56 -0300

    feat: add setupRoutes

---
+++ src/main/config/routes.ts
@@ -0,0 +1,13 @@
+import { readdirSync } from "fs";
+import { Express, Router } from "express";
+import { join } from "path";
+
+export default (app: Express): void => {
+	const router = Router();
+	app.use("/api", router);
+	readdirSync(join(__dirname, "../routes")).map(async (file) => {
+		if (!file.endsWith(".map")) {
+			(await import(`../routes/${file}`)).default(router);
+		}
+	});
+};
