b37481aa61b29fb8d29fa9b5c8f51b558dfc55b6
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-10-17 14:58:57 -0300

    chore: change local mongo url

--- src/infra/db/mongodb/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account-mongo-repository.spec.ts
@@ -6,7 +6,7 @@ describe("Account Mongo Repository", () => {
 	let accountCollection: Collection;
 	beforeAll(async () => {
 		await MongoHelper.connect(
-			process.env.MONGO_URL || "mongodb://localhost:27017/clean-node-api"
+			process.env.MONGO_URL || "mongodb://127.0.0.1:27017/rancho-api"
 		);
 	});
 
