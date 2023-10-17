05871fed8f98a63bcf5248dfc3f65969e0955777
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-10-17 16:35:36 -0300

    fix: error description and name

--- src/presentation/errors/invalid-param-error.ts
+++ src/presentation/errors/invalid-param-error.ts
@@ -1,6 +1,6 @@
 export class InvalidParamError extends Error {
 	constructor(param: string) {
-		super(`missing param: ${param}`);
-		this.name = "MissingParamError";
+		super(`invalid param: ${param}`);
+		this.name = "InvalidParamError";
 	}
 }
