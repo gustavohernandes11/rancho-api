0ad8e4418dbc4277613d9779736099cf967a46e3
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-11-02 09:52:15 -0300

    test: ensure basic usage in add animals route

---
+++ src/main/routes/animal-routes.test.ts
@@ -0,0 +1,78 @@
+import request from "supertest";
+import { Express } from "express";
+import { setupApp } from "../config/app";
+import { Collection } from "mongodb";
+import { MongoHelper } from "../../infra/db/mongodb/mongo-helper";
+import { sign } from "jsonwebtoken";
+import env from "../config/env";
+jest.setTimeout(15000);
+
+let app: Express;
+let accountCollection: Collection;
+describe("Animal routes", () => {
+	beforeAll(async () => {
+		app = await setupApp();
+		await MongoHelper.connect(process.env.MONGO_URL!);
+	});
+
+	afterAll(async () => {
+		await MongoHelper.disconnect();
+	});
+
+	beforeEach(async () => {
+		accountCollection = MongoHelper.getCollection("accounts");
+		await accountCollection.deleteMany({});
+	});
+
+	interface IMockDatabaseUserType {
+		userId: string;
+		accessToken: string;
+	}
+	const mockDatabaseUser = async (): Promise<IMockDatabaseUserType> => {
+		const { insertedId } = await accountCollection.insertOne({
+			name: "any_name",
+			email: "any.email@gmail.com",
+			password: "123",
+		});
+		const id = insertedId.toHexString();
+		const accessToken = sign({ id }, env.jwtSecret);
+		await accountCollection.updateOne(
+			{
+				_id: insertedId,
+			},
+			{
+				$set: {
+					accessToken,
+				},
+			}
+		);
+		return { accessToken, userId: id };
+	};
+
+	describe("/api/animals", () => {
+		it("should return 200 when sending valid animal data", async () => {
+			const { accessToken, userId } = await mockDatabaseUser();
+			console.log(accessToken);
+			console.log(userId);
+			await request(app)
+				.post("/api/animals")
+				.set("x-access-token", accessToken)
+				.send({
+					name: "any_name",
+					ownerId: userId,
+					age: "any_age",
+				})
+				.expect(200);
+		});
+		it("should return 400 if the animal owner is not in the database", async () => {
+			await request(app)
+				.post("/api/animals")
+				.send({
+					name: "any_name",
+					ownerId: "any_id",
+					age: "any_age",
+				})
+				.expect(400);
+		});
+	});
+});
