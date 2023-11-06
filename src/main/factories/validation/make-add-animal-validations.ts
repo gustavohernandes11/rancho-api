d649354481ac90686de2bd8ad0a810df1f6c3f32
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-11-06 14:51:51 -0300

    fix: add date format validation to add-animal-controller

--- src/main/factories/validation/make-add-animal-validations.ts
+++ src/main/factories/validation/make-add-animal-validations.ts
@@ -2,12 +2,14 @@ import {
 	RequiredFieldValidation,
 	ValidationComposite,
 } from "@validation/validators";
+import { GlobalDateFormatValidation } from "@validation/validators/global-date-field-validation";
 
 export const makeAddAnimalValidations = () => {
 	const validations = [
 		new RequiredFieldValidation("name"),
 		new RequiredFieldValidation("ownerId"),
 		new RequiredFieldValidation("age"),
+		new GlobalDateFormatValidation("age"),
 	];
 
 	return new ValidationComposite(validations);
