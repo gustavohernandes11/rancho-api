7acd096118e1db3afd34aeeaad7cbb557cc5699a
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-11-07 14:20:01 -0300

    feat: implement all the repositories related to batches

---
+++ src/infra/db/mongodb/batch-mongo-repository.spec.ts
@@ -0,0 +1,183 @@
+import { Collection } from "mongodb";
+import { BatchMongoRepository } from "./batch-mongo-repository";
+import { MongoHelper } from "./mongo-helper";
+import { IAddBatchModel } from "@domain/usecases/batch/add-batch";
+import { IUpdateBatchModel } from "@domain/usecases/batch/update-batch";
+
+const makeFakeBatch = (): IAddBatchModel => ({
+	name: "any_batch_name",
+	ownerId: "any_owner_id",
+});
+
+describe("Batch Mongo Repository", () => {
+	let batchesCollection: Collection;
+	let accountCollection: Collection;
+
+	beforeAll(async () => {
+		await MongoHelper.connect(
+			process.env.MONGO_URL || "mongodb://127.0.0.1:27017/rancho-api"
+		);
+	});
+
+	afterAll(async () => {
+		await MongoHelper.disconnect();
+	});
+
+	beforeEach(async () => {
+		batchesCollection = MongoHelper.getCollection("batches");
+		accountCollection = MongoHelper.getCollection("batches");
+		await batchesCollection.deleteMany({});
+		await accountCollection.deleteMany({});
+	});
+
+	describe("addBatch()", () => {
+		it("should return true if the batch was added", async () => {
+			const sut = new BatchMongoRepository();
+
+			const result = await sut.addBatch(makeFakeBatch());
+			expect(result).toBeTruthy();
+		});
+	});
+
+	describe("checkByName()", () => {
+		it("should return true when the batch exists in the database with this name", async () => {
+			const sut = new BatchMongoRepository();
+			await batchesCollection.insertOne({
+				name: "any_batch_name",
+				ownerId: "any_ownerId",
+			});
+			const result = await sut.checkByName("any_batch_name");
+			expect(result).toBeTruthy();
+		});
+		it("should return false when the batch do not exists in the database with this name", async () => {
+			const sut = new BatchMongoRepository();
+			const result = await sut.checkByName("invalid_batch_name");
+			expect(result).toBeFalsy();
+		});
+	});
+
+	describe("checkById()", () => {
+		it("should return true when the batch exists in the database with this ID", async () => {
+			const sut = new BatchMongoRepository();
+			const { insertedId } = await batchesCollection.insertOne({
+				name: "any_batch_name",
+				ownerId: "any_ownerId",
+			});
+			const result = await sut.checkById(insertedId.toHexString());
+			expect(result).toBeTruthy();
+		});
+		it("should return false when the batch do not exists in the database with this ID", async () => {
+			const sut = new BatchMongoRepository();
+			const result = await sut.checkById("invalid_batch_name");
+			expect(result).toBeFalsy();
+		});
+	});
+
+	describe("listBatches()", () => {
+		it("should return falsy if the ownerId is not valid", async () => {
+			const sut = new BatchMongoRepository();
+
+			const result = await sut.listBatches("non_existent_owner_id");
+
+			expect(result).toHaveLength(0);
+		});
+
+		it("should return an array of batches from the correct ownerId", async () => {
+			const sut = new BatchMongoRepository();
+
+			const ownerId = "valid_owner_id";
+			await batchesCollection.insertMany([
+				{
+					name: "batch_1",
+					ownerId,
+				},
+				{
+					name: "batch_2",
+					ownerId,
+				},
+				{
+					name: "batch_3",
+					ownerId: "any_other_ownerId",
+				},
+			]);
+
+			const result = await sut.listBatches(ownerId);
+
+			expect(Array.isArray(result)).toBe(true);
+			expect(result.length).toBe(2);
+		});
+	});
+
+	describe("removeBatch()", () => {
+		it("should remove the batch from the database", async () => {
+			const sut = new BatchMongoRepository();
+			const { insertedId } = await batchesCollection.insertOne(
+				makeFakeBatch()
+			);
+			const result = await sut.removeBatch(insertedId.toHexString());
+			expect(result).toBeTruthy();
+		});
+		it("should return false if the batch does not exist", async () => {
+			const sut = new BatchMongoRepository();
+			const result = await sut.removeBatch("non_existent_id");
+			expect(result).toBeFalsy();
+		});
+
+		it("should return false if exists batches but with other ID", async () => {
+			const sut = new BatchMongoRepository();
+
+			await batchesCollection.insertOne(makeFakeBatch());
+			const result = await sut.removeBatch("wrong_id");
+			expect(result).toBeFalsy();
+		});
+	});
+
+	describe("updateBatch()", () => {
+		it("should update the batch with the given ID", async () => {
+			const sut = new BatchMongoRepository();
+			const { insertedId } = await batchesCollection.insertOne(
+				makeFakeBatch()
+			);
+			const updateData: IUpdateBatchModel = {
+				name: "updated_name",
+				ownerId: "updated_owner_id",
+			};
+			const result = await sut.updateBatch(
+				insertedId.toHexString(),
+				updateData
+			);
+			expect(result).not.toBeNull();
+			const updatedBatch = await batchesCollection.findOne({
+				_id: insertedId,
+			});
+			expect(updatedBatch).toEqual(expect.objectContaining(updateData));
+		});
+
+		it("should return null if the batch with the given ID does not exist", async () => {
+			const sut = new BatchMongoRepository();
+			const updateData: IUpdateBatchModel = {
+				name: "updated_name",
+				ownerId: "updated_owner_id",
+			};
+			const result = await sut.updateBatch("non_existent_id", updateData);
+			expect(result).toBeNull();
+		});
+		it("should return the updated batch when success", async () => {
+			const sut = new BatchMongoRepository();
+			const { insertedId } = await batchesCollection.insertOne(
+				makeFakeBatch()
+			);
+			const updateData: IUpdateBatchModel = {
+				name: "updated_name",
+				ownerId: "updated_owner_id",
+			};
+			const result = await sut.updateBatch(
+				insertedId.toHexString(),
+				updateData
+			);
+			expect(result?.name).toBe("updated_name");
+			expect(result?.ownerId).toBe("updated_owner_id");
+			expect(result?.id).toBeTruthy();
+		});
+	});
+});
---
+++ src/infra/db/mongodb/batch-mongo-repository.ts
@@ -0,0 +1,83 @@
+import { IAddBatchRepository } from "@data/protocols/db/batch/add-batch-repository";
+import { ICheckBatchByIdRepository } from "@data/protocols/db/batch/check-batch-by-id-repository";
+import { ICheckBatchByNameRepository } from "@data/protocols/db/batch/check-batch-by-name-repository";
+import { IListAnimalsByBatchRepository } from "@data/protocols/db/animals/list-animals-by-batch-repository";
+import { IListBatchesByOwnerIdRepository } from "@data/protocols/db/batch/list-batches-repository";
+import { IRemoveBatchByIdRepository } from "@data/protocols/db/batch/remove-batch-by-id-repository";
+import { IUpdateBatchByIdRepository } from "@data/protocols/db/batch/update-batch-by-id-repository copy";
+import { IAddBatchModel } from "@domain/usecases/batch/add-batch";
+import { ObjectId } from "mongodb";
+import { IBatchModel } from "@domain/models/batch";
+import { IUpdateBatchModel } from "@domain/usecases/batch/update-batch";
+import { MongoHelper } from "./mongo-helper";
+import { parseToObjectId } from "./utils/parse-to-object-id";
+
+export class BatchMongoRepository
+	implements
+		IAddBatchRepository,
+		ICheckBatchByNameRepository,
+		ICheckBatchByIdRepository,
+		IListBatchesByOwnerIdRepository,
+		IRemoveBatchByIdRepository,
+		IUpdateBatchByIdRepository
+{
+	async checkByName(name: string): Promise<boolean> {
+		const batchesCollection = MongoHelper.getCollection("batches");
+		const batch = await batchesCollection.findOne({ name });
+		return !!batch;
+	}
+
+	async listBatches(ownerId: string): Promise<IBatchModel[]> {
+		const batchesCollection = MongoHelper.getCollection("batches");
+
+		const result = (await batchesCollection
+			.aggregate([{ $match: { ownerId } }])
+			.toArray()) as IBatchModel[];
+
+		return MongoHelper.mapCollection(result);
+	}
+	async addBatch(batch: IAddBatchModel): Promise<boolean> {
+		const batchesCollection = MongoHelper.getCollection("batches");
+		const { acknowledged } = await batchesCollection.insertOne(batch);
+		return acknowledged;
+	}
+
+	async checkById(id: string): Promise<boolean> {
+		const batchesCollection = MongoHelper.getCollection("batches");
+		const batch = await batchesCollection.findOne({
+			_id: parseToObjectId(id),
+		});
+		return !!batch;
+	}
+
+	async removeBatch(batchId: string | ObjectId): Promise<boolean> {
+		const batchesCollection = MongoHelper.getCollection("batches");
+		const { deletedCount } = await batchesCollection.deleteOne({
+			_id: parseToObjectId(batchId),
+		});
+		return deletedCount > 0;
+	}
+	async updateBatch(
+		id: string,
+		props: IUpdateBatchModel
+	): Promise<IBatchModel | null> {
+		const batchesCollection = MongoHelper.getCollection("batches");
+		const parsedId = parseToObjectId(id);
+		const { ok } = await batchesCollection.findOneAndUpdate(
+			{ _id: parsedId },
+			{
+				$set: props,
+			},
+			{
+				upsert: false,
+			}
+		);
+		if (ok) {
+			const updated = await batchesCollection.findOne({
+				_id: parseToObjectId(id),
+			});
+			if (updated) return MongoHelper.map(updated);
+		}
+		return null;
+	}
+}
