390d06470d3b03284f9aa9efeb81a0a3fdc7391a
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-11-09 20:40:13 -0300

    feat: add controller to list animals from a specific batch

---
+++ src/presentation/controllers/batch/load-batch-controller.ts
@@ -0,0 +1,26 @@
+import {
+	noContent,
+	notFound,
+	ok,
+	serverError,
+} from "../../helpers/http-helpers";
+import { IController, IHttpRequest } from "@presentation/protocols";
+import { IDbListAnimalsByBatch } from "@domain/usecases/batch/list-animals-by-batch";
+
+export class LoadBatchController implements IController {
+	constructor(private readonly dbListAnimalsByBatch: IDbListAnimalsByBatch) {}
+
+	async handle(request: IHttpRequest) {
+		try {
+			const { batchId } = request as any;
+			const batches = await this.dbListAnimalsByBatch.list(batchId);
+
+			if (batches?.length === 0) return noContent();
+
+			if (batches === null) return notFound();
+			return ok(batches);
+		} catch (error) {
+			return serverError(error as Error);
+		}
+	}
+}
