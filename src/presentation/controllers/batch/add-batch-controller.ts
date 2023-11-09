af7af646b5b878486ddbe88ac5897ff6528cde46
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-11-09 20:41:51 -0300

    feat: add controller to add a batch

---
+++ src/presentation/controllers/batch/add-batch-controller.ts
@@ -0,0 +1,31 @@
+import { IDbAddAnimal } from "@domain/usecases/add-animal";
+import { InvalidParamError } from "../../errors";
+import { badRequest, ok, serverError } from "../../helpers/http-helpers";
+import {
+	IController,
+	IHttpRequest,
+	IHttpResponse,
+	IValidation,
+} from "../../protocols";
+import { IDbAddBatch } from "@domain/usecases/batch/add-batch";
+
+export class AddBatchController implements IController {
+	constructor(
+		private readonly validations: IValidation,
+		private readonly dbAddBatch: IDbAddBatch
+	) {}
+	async handle(request: IHttpRequest): Promise<IHttpResponse> {
+		try {
+			const error = this.validations.validate(request.body);
+			if (error) return badRequest(error);
+
+			const { name, ownerId } = request.body;
+
+			const wasAdded = await this.dbAddBatch.add({ name, ownerId });
+			if (!wasAdded) return badRequest(new InvalidParamError("ownerId"));
+			return ok();
+		} catch (error) {
+			return serverError(error as Error);
+		}
+	}
+}
