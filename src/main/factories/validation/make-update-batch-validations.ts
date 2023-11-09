0be10f6f5497a4d3c9b9ddb43a1b22e4447a15bb
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-11-09 20:45:56 -0300

    feat: add factory functions to create validations to add and update batch controllers

---
+++ src/main/factories/validation/make-update-batch-validations.ts
@@ -0,0 +1,13 @@
+import { IValidation } from "@presentation/protocols";
+import { ValidationComposite } from "@validation/validators";
+import { AtLeastOneOfTheseFieldsValidation } from "@validation/validators/at-least-one-of-these-fields-validation";
+
+export const makeUpdateBatchValidations = () => {
+	const validations: IValidation[] = [];
+
+	validations.push(
+		new AtLeastOneOfTheseFieldsValidation(["ownerId", "name"])
+	);
+
+	return new ValidationComposite(validations);
+};
