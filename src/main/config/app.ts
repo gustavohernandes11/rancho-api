af43099dd025f5054de17c984588d51968276e09
gustavohernandes11 <gustavo.hernandes.s11@gmail.com>
2023-12-24 11:31:44 -0300

    docs: setup swagger

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,12 +1,14 @@
 import express, { Express } from "express";
 import setupMiddlewares from "./middlewares";
 import setupRoutes from "./routes";
+import setupSwagger from "./swagger";
 
 export const setupApp = async (): Promise<Express> => {
 	const app = express();
 
 	setupMiddlewares(app);
 	setupRoutes(app);
+	setupSwagger(app);
 
 	return app;
 };
